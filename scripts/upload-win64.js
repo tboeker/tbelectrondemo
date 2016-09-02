d4fa717c1ad54c7fe76d09d9960ab0fc5c2756ff
Tobias Böker Work <tb@susdv.de>
2016-09-02 08:53:26 +0200

    ...

--- scripts/upload-win64.js
+++ scripts/upload-win64.js
@@ -1,27 +1,24 @@
-//var glob = require("glob")
 var glob = require("multi-glob").glob;
-var ghReleaseAssets = require('gh-release-assets')
 var publishRelease = require('publish-release')
-var pkginfo = require('pkginfo')(module, 'version','name')
+var pkginfo = require('pkginfo')(module, 'version', 'name')
 
 console.log("package version: ", module.exports.version)
 
 var tag = "v" + module.exports.version
 console.log("tag: ", tag)
 
-var releaseName = "Release " + module.exports.name + " Version: " + module.exports.version 
+var releaseName = "Release " + module.exports.name + " Version: " + module.exports.version
 console.log("releasename: ", releaseName)
 
-var pattern1 = "dist/installers/win32-x64/RELEASES*"
+var pattern1 = "dist/win/RELEASES*"
 console.log("pattern1", pattern1)
-var pattern2 = "dist/installers/win32-x64/**/*" + module.exports.version + "*.exe"
+var pattern2 = "dist/win/**/*" + module.exports.version + "*.exe"
 console.log("pattern2", pattern2)
-var pattern3 = "dist/installers/win32-x64/**/*" + module.exports.version + "*.nupkg"
+var pattern3 = "dist/win/**/*" + module.exports.version + "*.nupkg"
 console.log("pattern3", pattern3)
 
 //var pattern = "dist/installers/win32-x64/**/!(*.msi)"
 //console.log("pattern", pattern)
-
 
 var demo = false;
 
@@ -59,45 +56,20 @@ else {
 
 
 glob([pattern1, pattern2, pattern3], function (err, matches) {
-//glob(pattern, function (err, matches) {
-
     if (err) throw err
-
-    // files is an array of filenames.
-    // If the `nonull` option is set, and nothing
-    // was found, then files is ["**/*.js"]
-    // er is an error object or null.
 
     console.log("matches", matches)
 
     if (demo == false) {
-        /* 
-          ghReleaseAssets({
-               url: "https://github.com/tboeker/tbelectrondemo",
-               token: token,
-               assets: matches
-           }, function (err, assets) {
-               console.log(assets)
-           }                
-           )
-           
-*/
-
-
         publishRelease({
-            /*   owner = "tboeker",
-             repo = "tbelectrondemo",
-             tag = "v" + module.exports.version , */
             token: token,
             assets: matches,
             owner: "tboeker",
             repo: "tbelectrondemo",
             tag: tag,
             reuseRelease: true
-           // name : releaseName
 
         }, function (err, release) {
-            // `release`: object returned from github about the newly created release
             if (err) {
                 console.error(err)
                 throw err
@@ -108,10 +80,6 @@ glob([pattern1, pattern2, pattern3], function (err, matches) {
         console.log("demo, do not upload");
     }
 
-    /*  matches.forEach(function (file) {
-          console.log("file:", file)
- 
- })*/
 
 })
 
