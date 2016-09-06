af491138e8eb777d0addffed360fe768cac3e916
Tobias Böker Work <tb@susdv.de>
2016-09-06 11:07:41 +0200

    ...

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 const logger = require('winston');
-logger.add(logger.transports.File, { filename: "app.log" });
+logger.add(logger.transports.File, {
+  filename: "app.log"
+});
 
 global.logger = logger;
 
@@ -7,7 +9,7 @@ const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
 
-app.setAppUserModelId("tb.demo.electron");
+//app.setAppUserModelId("tb.demo.electron");
 
 //https://medium.com/@svilen/auto-updating-apps-for-windows-and-osx-using-electron-the-complete-guide-4aa7a50b904c#.wpoukxnlq
 if (require('electron-squirrel-startup')) {
@@ -21,8 +23,7 @@ if (handleSquirrelEvent()) {
   // squirrel event handled and app will exit in 1000ms, so don't do anything else
   logger.info("handleSquirrelEvent. return");
   return;
-}
-else {
+} else {
   logger.info("no handleSquirrelEvent. continue");
 }
 
@@ -87,7 +88,7 @@ updater.check((err, status) => {
     logger.info("updater", "updater.check. start download");
 
     // Download the update
-    updater.download()    
+    updater.download()
   }
 })
 
@@ -102,8 +103,8 @@ updater.on('update-downloaded', (info) => {
 
 
 logger.info('start autoUpdater. currentVersion:' + app.getVersion())
- // Access electrons autoUpdater
- updater.autoUpdater
+  // Access electrons autoUpdater
+updater.autoUpdater
 
 
 // Module to create native browser window.
@@ -117,7 +118,7 @@ const BrowserWindow = electron.BrowserWindow
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -125,7 +126,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -139,7 +140,10 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
@@ -149,7 +153,7 @@ function createWindow() {
 
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -174,18 +178,20 @@ function handleSquirrelEvent() {
   const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
   const exeName = path.basename(process.execPath);
 
-  const spawn = function (command, args) {
+  const spawn = function(command, args) {
     let spawnedProcess, error;
 
     try {
-      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+      spawnedProcess = ChildProcess.spawn(command, args, {
+        detached: true
+      });
     } catch (error) {
       logger.log('eror', error);
     }
     return spawnedProcess;
   };
 
-  const spawnUpdate = function (args) {
+  const spawnUpdate = function(args) {
     return spawn(updateDotExe, args);
   };
 
