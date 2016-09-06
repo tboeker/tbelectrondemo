72abb45e4cfde981099b7c1a19037e13d9e3eddb
Tobias Böker <tobias.boeker@outlook.com>
2016-09-07 00:02:32 +0200

    ::

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/electron-builder/index.d.ts" />
+/// <reference path="globals/electron-packager/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/rimraf/index.d.ts" />
+/// <reference path="globals/winston/index.d.ts" />
