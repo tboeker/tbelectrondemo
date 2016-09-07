490928f45bb245fb2f579908fbae3ac8a13e8818
Tobias Böker Work <tb@susdv.de>
2016-09-07 09:27:43 +0200

    ..

--- scripts/dist-win64.js
+++ scripts/dist-win64.js
@@ -1,58 +1,38 @@
 var electronInstaller = require('electron-winstaller');
 var pkginfo = require('pkginfo')(module, 'version', 'name')
 
-console.log("package version: ", module.exports.version)
+console.log('package version: ', module.exports.version)
 
-var exe = module.exports.name + "-" + module.exports.version + "-win32-setup-x64.exe"
-console.log("exe name:", exe)
+var exe = module.exports.name + '-' + module.exports.version + '-win32-setup-x64.exe'
+console.log('exe name:', exe)
 
-var certificateFile = "./tbdemoelectron.pfx";
+var certificateFile = './tbdemoelectron.pfx';
 if (!process.env.CERT_PASSWORD) {
-    certificateFile = "";
+    console.log('no cert password');
+    certificateFile = '';
 }
 
+var remoteReleases = 'https://github.com/tboeker/tbelectrondemo';
+if (!process.env.GH_REPO)
+{
+    console.log('remoteReleases', process.env.GH_REPO);
+    remoteReleases = process.env.GH_REPO;
+}
 
-resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: "./dist/tbdemoelectron-win32-x64",
-    outputDirectory: "./dist/installer/win32-x64",
-    loadingGif: "./resources/install-spinner.gif",
-    setupIcon: "./resources/icon.ico",
-    iconUrl: "https://raw.githubusercontent.com/tboeker/tbelectrondemo/master/resources/icon.ico",
+var options = {
+    appDirectory: './dist/tbdemoelectron-win32-x64',
+    outputDirectory: './dist/installer/win32-x64',
+    loadingGif: './resources/install-spinner.gif',
+    setupIcon: './resources/icon.ico',
+    iconUrl: 'https://raw.githubusercontent.com/tboeker/tbelectrondemo/master/resources/icon.ico',
     setupExe: exe,
     noMsi: true,
     certificateFile: certificateFile,
-    certificatePassword: process.env.CERT_PASSWORD || "",
-    remoteToken: process.env.GH_TOKEN || "",
-    remoteReleases : "https://github.com/tboeker/tbelectrondemo"
-});
-
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
+    certificatePassword: process.env.CERT_PASSWORD || '',
+    remoteToken: process.env.GH_TOKEN || '',
+    remoteReleases : remoteReleases
+};
 
 
-/*
-
-var createInstaller = require('electron-installer-squirrel-windows')
-
-var opts = {
-    path: "./dist/tbdemoelectron-win32-x64",
-    out: "./dist/installer2/win32-x64",
-    loadingGif: "./resources/install-spinner.gif",
-    setupIcon: "./resources/icon.ico",
-    iconUrl: "https://raw.githubusercontent.com/tboeker/tbelectrondemo/master/resources/icon.ico",
-    setupExe: exe,
-    noMsi: true,
-    authors :"TB"
-}
-
-createInstaller(opts, function done(err) {
-
-    if (err) {
-        console.error(err)
-        throw err
-    }
-
-    console.log("it worked 2");
-
-})
-
-*/
+resultPromise = electronInstaller.createWindowsInstaller(options);
+resultPromise.then(() => console.log('It worked!'), (e) => console.log(`No dice: ${e.message}`));
